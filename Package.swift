0482e6efd11d78613abbcc29a6e18015e10e7549
Marat Khuzhayarov <Khuzhayarov.M.Tem@sberbank.ru>
2020-12-14 18:36:38 +0300

    removed compression

--- Package.swift
+++ Package.swift
@@ -27,13 +27,8 @@ let package = Package(
         products: [
             .library(name: "Starscream", targets: ["Starscream"])
         ],
-        dependencies: [],
         targets: [
             .target(name: "Starscream",
                     path: "Sources")
         ]
 )
-
-#if os(Linux)
-    package.dependencies.append(.package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0"))
-#endif
